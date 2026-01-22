a2d5dce75c1aefdb1e0599ed1cde19dd98164d7f
Victor San Kho Lin <victor@sankholin.com>
2026-01-22 18:33:38 +1100

    Added tftest hcl terraform test

---
+++ tftest-hcl/mock-provider/main.tf
@@ -0,0 +1,17 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+}
